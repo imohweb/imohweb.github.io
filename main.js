efbd354244c4f87c033ed0c2504c8c398e4adda9
Imoh Etuk <39197873+imohweb@users.noreply.github.com>
2020-03-05 01:41:01 +0100

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ function calculate()
       r= document.getElementById("r").value;
       n= document.getElementById("n").value;
       result= document.getElementById("result");
-      result.innerHTML="The interest for the specify No.of years is " + (p*n*r/100);
+      result.innerHTML="The interest is " + (p*n*r/100);
 
      }
     function myFunction() {
